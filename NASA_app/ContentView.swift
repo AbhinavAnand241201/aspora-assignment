74a03e760ab635075051433993d1a41240b9f4c3
AbhinavAnand241201 <iabhinavanand10@gmail.com>
2025-12-15 14:30:32 +0530

    Initial Commit

---
+++ NASA_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NASA_app
+//
+//  Created by ABHINAV ANAND  on 15/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NASA_app/NASA_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASA_appApp.swift
+//  NASA_app
+//
+//  Created by ABHINAV ANAND  on 15/12/25.
+//
+
+import SwiftUI
+
+@main
+struct NASA_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
